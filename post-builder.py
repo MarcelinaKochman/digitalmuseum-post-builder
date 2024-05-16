58ba60c65a4780d771e2ab6ebaa798c8a8708a22
marcelinakochman <kochmanmarcelina@gmail.com>
2024-05-16 21:23:28 +0200

    Init commit

---
+++ post-builder.py
@@ -0,0 +1,87 @@
+import shutil
+import sys
+from pathlib import Path
+
+import instaloader
+from instaloader import Post
+from PIL import Image, ImageFont, ImageDraw
+
+TOP_MARGIN = 81
+
+LEFT_MARGIN = 138
+
+WHITE = (255, 255, 255)
+IMAGE_SIZE = 1024
+MAX_PHOTO_SIZE = 757
+POST_DIRECTORY_PATH = "post"
+
+
+def list_files_in_folder_with_extension(folder_path, extension):
+    folder = Path(folder_path)
+    files = [str(file) for file in folder.glob('**/*') if file.is_file()
+             and file.suffix == extension]
+    return files
+
+
+def create_instagram_post():
+    global L, post
+    L = instaloader.Instaloader()
+    postShortcode = sys.argv[1]
+    post = Post.from_shortcode(L.context, postShortcode)
+    return post
+
+
+def resize_image(original_width, original_height, max_size):
+    # Calculate aspect ratio
+    aspect_ratio = original_width / original_height
+
+    # Determine which side is longer
+    if original_width > original_height:
+        # If width is longer, calculate new width and height based on max_size
+        new_width = max_size
+        new_height = int(max_size / aspect_ratio)
+    else:
+        # If height is longer or they are equal, calculate new width and height based on max_size
+        new_height = max_size
+        new_width = int(max_size * aspect_ratio)
+
+    return new_width, new_height
+
+
+def calculate_photo_position(photo_width, photo_height, min_top_margin, max_photo_size, background_width):
+    # Calculate margins
+    x = (background_width - photo_width) // 2  # Center horizontally
+    y = min_top_margin + ((max_photo_size - photo_height) // 2)  # Ensure minimum top margin of 81 pixels
+
+    # Ensure minimum left margin of 138 pixels
+    return x, y
+
+
+post = create_instagram_post()
+L.download_post(post, target=POST_DIRECTORY_PATH)
+
+username = post.owner_profile.username
+title = sys.argv[2]
+
+imagesPaths = list_files_in_folder_with_extension(POST_DIRECTORY_PATH, ".jpg")
+
+for path in imagesPaths:
+    resultImage = Image.new("RGB", (IMAGE_SIZE, IMAGE_SIZE), WHITE)
+    photo = Image.open(path)
+    new_width, new_height = resize_image(photo.width, photo.height, MAX_PHOTO_SIZE)
+    photo = photo.resize((new_width, new_height))
+    x, y = calculate_photo_position(new_width, new_height, TOP_MARGIN, MAX_PHOTO_SIZE, IMAGE_SIZE)
+
+    fontItalic = ImageFont.truetype("font/PoltawskiNowy-Italic.ttf", 24)
+    fontBold = ImageFont.truetype("font/PoltawskiNowy-Bold.ttf", 24)
+
+    draw = ImageDraw.Draw(im=resultImage)
+    draw.text(xy=(LEFT_MARGIN, 920), text="@" + username, font=fontItalic, fill='black', align='left')
+    draw.text(xy=(LEFT_MARGIN, 885), text=title, font=fontBold, fill='black', align='left')
+
+    resultImage.paste(photo, (x, y))
+    resultImage.save("output/" + path)
+
+
+shutil.rmtree(POST_DIRECTORY_PATH)
+
